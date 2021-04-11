6aac9ed68a47bcb38f9ecbd3cc4c598a1a9d7bb4
Konstantinos Drosos <dean.drossos@gmail.com>
2021-04-11 11:23:28 -0400

    Fixed graphs but still scroller has an issue

--- main2.py
+++ main2.py
@@ -15,7 +15,8 @@ class MainApp(tk.Frame):
         self.revenues = DataAnalysis()
         self.expenses = ExpensesAnalysis()
         self.master = master
-        self.master.geometry('800x600')
+        self.master.title('Bankrupt Smart')
+        self.master.geometry('1200x600')
         self.wrapper1 = tk.LabelFrame(self.master)
         self.canvas = self.create_canvas(self.wrapper1)
         self.scroller = self.create_scrollbar(self.wrapper1, self.canvas, tk.VERTICAL)
@@ -23,7 +24,14 @@ class MainApp(tk.Frame):
         self.frame = tk.Frame(self.canvas)
         self.canvas.create_window((0, 0), window=self.frame, anchor='nw')
         self.wrapper1.pack(fill='both', expand='yes', padx=10, pady=10)
-        self.create_revenues_charts()
+        self.create_revenues_charts('revenues', 'Timeline', 'Income_Value', 'Monthly Revenues', 'Income',
+                                    'Month', 0, 1)
+        self.create_revenues_charts('assets', 'Time', 'Value', 'Total Assets Value', 'Assets Value',
+                                    'Month', 0, 2)
+        self.create_expenses_charts('bills', 'Month_Year', 'Monthly Cost', 'Monthly Bills', 'Monthly Cost',
+                                    'Month', 1, 1)
+        self.create_expenses_charts('debt', 'Month_Year', 'Total Cost', 'Total Debt', 'Monthly_Cost', 'Month',
+                                    2, 1)
 
     # Create a canvas and add a scroller
     def create_canvas(self, label_frame):
@@ -38,15 +46,44 @@ class MainApp(tk.Frame):
         return scroll
 
     # Create the revenues charts
-    def create_revenues_charts(self):
-        data = self.revenues.get_income_data('revenues')
+    def create_revenues_charts(self, category, x_val, y_val, chart_title, x_label, y_label, col_pos, row_pos):
+        data = self.revenues.get_income_data(category)
         figure = plt.Figure(figsize=(4, 4), dpi=100)
-        ax1 = figure.add_subplot(111)
-        bar1 = FigureCanvasTkAgg(figure, self.frame)
-        bar1.get_tk_widget().pack(side=tk.LEFT, fill='y')
-        df1 = data[['Timeline', 'Income_Value']].groupby('Timeline').sum()
-        df1.plot(kind='bar', legend=True, ax=ax1)
-        ax1.set_title('Monthly Income')
+        y_value = data[y_val]
+        x_value = data[x_val]
+
+        # Plotting the graph
+        graph = figure.add_subplot(111)
+        graph.bar(x_value, y_value, 10)
+        graph.set_xlabel(x_label)
+        graph.set_ylabel(y_label)
+        graph.set_title(chart_title)
+        graph.grid()
+
+        # Adding everything on canvas
+        chart = FigureCanvasTkAgg(figure, master=self.canvas)
+        chart.draw()
+        chart.get_tk_widget().grid(column=col_pos, row=row_pos, padx=10, pady=10)
+
+    # Create expenses report
+    def create_expenses_charts(self, category, x_val, y_val, chart_title, x_label, y_label, col_pos, row_pos):
+        data = self.expenses.get_type_expense(category)
+        figure = plt.Figure(figsize=(4, 4), dpi=100)
+        y_values = data[y_val]
+        x_values = data[x_val]
+
+        # Plotting the graph
+        graph = figure.add_subplot(111)
+        graph.bar(x_values, y_values, 10)
+        graph.set_xlabel(x_label)
+        graph.set_ylabel(y_label)
+        graph.set_title(chart_title)
+        graph.grid()
+
+        # adding everything in the canvas
+        chart = FigureCanvasTkAgg(figure, master=self.canvas)
+        chart.draw()
+        chart.get_tk_widget().grid(column=col_pos, row=row_pos, padx=10, pady=10)
 
 
 root = tk.Tk()
